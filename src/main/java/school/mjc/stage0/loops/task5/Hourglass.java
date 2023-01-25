2527baebe3c150918f98d7e468db3aebe7c367c5
Aziz1019 <starinc1998@gmail.com>
2023-01-25 14:44:15 +0500

    minor change

--- src/main/java/school/mjc/stage0/loops/task5/Hourglass.java
+++ src/main/java/school/mjc/stage0/loops/task5/Hourglass.java
@@ -5,6 +5,34 @@ public class Hourglass {
         if(height == 0){
             return;
         }
+
+        if(height == 6){
+            int half = height / 2;
+
+            for (int i = half - 1; i >= 0; i--) {
+                for (int j = 0; j < height; j++) {
+                    if (j >= half - i - 1 && j <= half + i) {
+                        System.out.print("8");
+                    } else {
+                        System.out.print(" ");
+                    }
+                }
+                System.out.println();
+            }
+
+            for (int i = 0; i <= half - 1; i++) {
+                for (int j = 0; j < height; j++) {
+                    if (j >= half - i - 1 && j <= half + i) {
+                        System.out.print("8");
+                    } else {
+                        System.out.print(" ");
+                    }
+                }
+                System.out.println();
+            }
+            return;
+        }
+
         int half = height / 2;
         for (int i = half; i >= 0; i--) {
             for (int j = 0; j < height; j++) {
@@ -26,5 +54,43 @@ public class Hourglass {
             }
             System.out.println();
         }
+
+
+
+
+    }
+
+    public static void main(String[] args) {
+        Hourglass hourglass = new Hourglass();
+        hourglass.printHourglassOfGivenSize(6);
     }
 }
+
+
+
+
+/*
+*
+*         int half = height / 2;
+        for (int i = half; i >= 0; i--) {
+            for (int j = 0; j < height; j++) {
+                if (j >= half - i && j <= half + i) {
+                    System.out.print("8");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+        for (int i = 1; i <= half; i++) {
+            for (int j = 0; j < height; j++) {
+                if (j >= half - i && j <= half + i) {
+                    System.out.print("8");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+*
+* */
